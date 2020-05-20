df1dce755be0d0744f7ace153d2922893808669c
normalsled <appliedboredom@gmail.com>
2020-05-20 00:21:27 -0500

    another card

--- src/components/Portfolio/Portfolio.js
+++ src/components/Portfolio/Portfolio.js
@@ -7,6 +7,7 @@ import Button from 'react-bootstrap/Button';
 import Image from 'react-bootstrap/Image';
 import sbheader from "../../sbrheader.png";
 import the_boys from "../../theboys.png";
+import spongebob from "../../spongebob.gif";
 import './portfolio.css'
 
 
@@ -22,6 +23,7 @@ class Portfolio extends React.Component {
     render() {
 
         return(
+           
             <div className="container-fluid d-flex align-items-center">
 
                     <Col className="col-md-2" id="left-col">
@@ -30,18 +32,18 @@ class Portfolio extends React.Component {
 
                     <Col className="col-lg-8" id="center-col">
                         <header>
-                            <h1 className="text-center" id="header">Ben Jerke's Coding Projects</h1>
+                            <h1 className="text-center text-color-white" id="header">Ben Jerke's Coding Projects</h1>
                         </header>
 
                         <Card className="text-center prjcard" id="superhero-battle-royale">
                             <Card.Body className="lowerborder bg-light">
                                 <Card.Title>Bootcamp Project 1: "Superhero Battle Royale"</Card.Title>
                                 <br/>
-                                <Image src={sbheader}/>
+                                <Image src={sbheader} fluid/>
                                 <br/>
                                 <br/>
                                 <Card.Text>
-                                This fun little website pits two superheroes against each other. <br/>
+                                This fun little website pits two superheroes against each other based on stats retrieved from an API. <br/>
                                 Finally, a way to settle arguments between superfans!
                                 </Card.Text>
                                 <Button variant="outline-primary" href="https://danhauen.github.io/Project1/">Try it out!</Button>
@@ -58,7 +60,7 @@ class Portfolio extends React.Component {
                                 <br/>
                                 <Card.Text className="text-align-left">
                                 I made this tool to help my fellow software testers who work on healthcare apps. <br/>
-                                Since I could never remember the format, I let the computer do it for me!
+                                
                                 </Card.Text>
                                 <Button variant="outline-primary" href="https://benjerke.github.io/MBI-Generator/">Try it out!</Button>
                             </Card.Body>
@@ -68,7 +70,7 @@ class Portfolio extends React.Component {
                             <Card.Body className="lowerborder bg-light">
                                 <Card.Title>Bootcamp Project 2: "Interest Quest"</Card.Title>
                                 <br/>
-                                <Image src={the_boys}/>
+                                <Image src={the_boys} fluid/>
                                 <br/>
                                 <br/>
                                 <Card.Text>
@@ -80,7 +82,23 @@ class Portfolio extends React.Component {
                             <Card.Footer className="text-muted">Developed Robert Forster, Dan Hauen, and Ben Jerke</Card.Footer>
                         </Card>
 
+                        <Card className="text-center prjcard" id="superhero-battle-royale">
+                            <Card.Body className="lowerborder bg-light">
+                                <Card.Title>Bootcamp Homework Assigment: "It's Gifs!"</Card.Title>
+                                <br/>
+                                <Image src={spongebob} fluid/>
+                                <br/>
+                                <br/>
+                                <Card.Text>
+                                A simple tool that returns a series of GIFs from the GIPHY API. 
+                                </Card.Text>
+                                <Button variant="outline-primary" href="https://benjerke.github.io/unit-6-homework/">Try it out!</Button>
+                            </Card.Body>
+                        </Card>
+
                     </Col>
+
+                    
 
                     
                     <Col className="col-md-2" id="right-col">
@@ -89,6 +107,7 @@ class Portfolio extends React.Component {
 
             
             </div>
+         
         )
 
     }
